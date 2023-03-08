482f8463d9e76c0ad599ffdda023f2563aa38e71
SimonDeVos <devos.simon@gmail.com>
2023-03-08 17:20:57 +0100

    include one-hot encoding

--- preprocessing/preprocessing.py
+++ preprocessing/preprocessing.py
@@ -1,14 +1,19 @@
 #TODO add import statements
+
 import numpy as np
 import pandas as pd
 import random
 from sklearn.preprocessing import StandardScaler
+from sklearn.preprocessing import OneHotEncoder
 from sklearn.manifold import TSNE
 import matplotlib.pyplot as plt
 import seaborn as sns
+import category_encoders as ce
 from category_encoders import woe
 import time
 
+import warnings
+warnings.filterwarnings('ignore', category=pd.errors.SettingWithCopyWarning)    #TODO: solve warning instead of suppressing it
 
 """Contents:
 import statements
@@ -31,14 +36,57 @@ random.seed(42)
 
 """3 GENERIC PREPROCESSING FUNCTIONS"""
 
-def convert_categorical_variables(x_train, y_train, x_val, x_test, categorical_variables):
+def convert_categorical_variables(x_train, y_train, x_val, x_test, categorical_variables, cat_encoder):
 
-    # Use weight of evidence encoding: WOE = ln (p(1) / p(0))
-    woe_encoder = woe.WOEEncoder(verbose=1, cols=categorical_variables)
-    woe_encoder.fit(x_train, y_train)
-    x_train = woe_encoder.transform(x_train)
-    x_val = woe_encoder.transform(x_val)
-    x_test = woe_encoder.transform(x_test)
+    if cat_encoder == 1:
+        """
+        onehot_encoder = OneHotEncoder() #TODO: verder aanvullen
+#        onehot_encoder.fit(x_train, y_train)
+        onehot_encoder.fit(x_train[categorical_variables])
+        
+        x_train = onehot_encoder.transform(x_train)
+        x_val = onehot_encoder.transform(x_val)
+        x_test = onehot_encoder.transform(x_test)
+        """
+        """
+        encoder = OneHotEncoder(handle_unknown='ignore')
+        x_train_cat_encoded = encoder.fit_transform(x_train[:, categorical_variables])
+        x_val_cat_encoded = encoder.transform(x_val[:, categorical_variables])
+        x_test_cat_encoded = encoder.transform(x_test[:, categorical_variables])
+
+        x_train_encoded = np.concatenate((x_train[:, [0]], x_train_cat_encoded.toarray()), axis=1)
+        x_val_encoded = np.concatenate((x_val[:, [0]], x_val_cat_encoded.toarray()), axis=1)
+        x_test_encoded = np.concatenate((x_test[:, [0]], x_test_cat_encoded.toarray()), axis=1)
+        """
+        """
+        numerical_variables = list(set(range(x_train.shape[1])) - set(categorical_variables))
+#        x_num = x_train[:, numerical_variables]  # numerical features
+#        x_cat = x_train[:, categorical_variables]  # categorical features
+
+        # encode categorical features using one-hot encoding
+        encoder = OneHotEncoder(handle_unknown='ignore')
+        x_train_cat_encoded = encoder.fit_transform(x_train[:, categorical_variables])
+        x_val_cat_encoded = encoder.transform(x_val[:, categorical_variables])
+        x_test_cat_encoded = encoder.transform(x_test[:, categorical_variables])
+
+        # concatenate numerical and encoded categorical features
+        x_train = np.concatenate((x_train[:, numerical_variables], x_train_cat_encoded.toarray()), axis=1)
+        x_val = np.concatenate((x_val[:, numerical_variables], x_val_cat_encoded.toarray()), axis=1)
+        x_test = np.concatenate((x_test[:, numerical_variables], x_test_cat_encoded.toarray()), axis=1)
+        """
+        ce_one_hot = ce.OneHotEncoder(cols=categorical_variables)   #TODO: question : what happens if categorical values exist in val or test, but not in train? How is this encoded?
+        ce_one_hot.fit(x_train)
+        x_train = ce_one_hot.transform(x_train)
+        x_val = ce_one_hot.transform(x_val)
+        x_test = ce_one_hot.transform(x_test)
+
+    elif cat_encoder == 2:
+        # Use weight of evidence encoding: WOE = ln (p(1) / p(0))
+        woe_encoder = woe.WOEEncoder(verbose=1, cols=categorical_variables)
+        woe_encoder.fit(x_train, y_train)
+        x_train = woe_encoder.transform(x_train)
+        x_val = woe_encoder.transform(x_val)
+        x_test = woe_encoder.transform(x_test)
 
     return x_train, x_val, x_test
 
@@ -61,7 +109,7 @@ def standardize(x_train, x_val, x_test):
 
 
 def handle_missing_data(df_train, df_val, df_test, categorical_variables):
-
+    #print(str(df_train.keys()))
     for key in df_train.keys():
         # If variable has > 90% missing values: delete
         if df_train[key].isna().mean() > 0.9:
@@ -76,15 +124,29 @@ def handle_missing_data(df_train, df_val, df_test, categorical_variables):
         # Handle other missing data:
         #   Categorical variables: additional category '-1'
         if key in categorical_variables:
-            df_train.loc[key] = df_train.loc[key].fillna('-1')
-            df_val.loc[key] = df_val.loc[key].fillna('-1')
-            df_test.loc[key] = df_test.loc[key].fillna('-1')
+            df_train[key].fillna('-1', inplace = True)
+            df_val[key].fillna('-1', inplace = True)
+            df_test[key].fillna('-1', inplace = True)
+
         #   Continuous variables: median imputation
         else:
-            median = df_train.loc[key].median()
-            df_train.loc[key] = df_train.loc[key].fillna(median)
-            df_val.loc[key] = df_val.loc[key].fillna(median)
-            df_test.loc[key] = df_test.loc[key].fillna(median)
+            median = df_train[key].median()
+#            median = df_train.loc[key].median()
+
+#            df_train.loc[key] = df_train.loc[key].fillna(median)
+#            df_train[key] = df_train[key].fillna(median)
+            df_train[key].fillna(median, inplace=True)
+#            df_train.loc[:, key] = df_train[key].fillna(median)
+
+#            df_val.loc[key] = df_val.loc[key].fillna(median)
+#            df_val[key] = df_val[key].fillna(median)
+            df_val[key].fillna(median, inplace=True)
+#            df_val.loc[:, key] = df_val[key].fillna(median)
+
+#            df_test.loc[key] = df_test.loc[key].fillna(median)
+#            df_test[key] = df_test[key].fillna(median)
+            df_test[key].fillna(median, inplace=True)
+#            df_test.loc[:, key] = df_test[key].fillna(median)
 
     assert df_train.isna().sum().sum() == 0 and df_val.isna().sum().sum() == 0 and df_test.isna().sum().sum() == 0
 
@@ -92,8 +154,53 @@ def handle_missing_data(df_train, df_val, df_test, categorical_variables):
 
 """DATASET-SPECIFIC PREPROCESSING FUNCTIONS"""
 
+# read data
+# Drop ID and useless columns
+# fill meaningful NaN
+# transform Turnover feature to 0/1
+# Split into covariates, labels
+# Create cost matrix
+# List categorical variables
+
+
 def preprocess_acerta():
-    return covariates, labels, amounts, cost_matric, categorical_variables
+    return covariates, labels, amounts, cost_matrix, categorical_variables
+
+def preprocess_eds():
+    #read data
+    try:
+        df = pd.read_csv('data/eds.csv', sep=',')
+    except FileNotFoundError:
+        df = pd.read_csv('../data/eds.csv', sep=',')
+
+    # Drop ID and useless columns
+
+    # fill meaningful NaN
+    df['filed_complaint'].fillna(0, inplace=True)
+    df['recently_promoted'].fillna(0, inplace=True)
+
+    # Transform 'Attrition' from Yes/No to 1/0
+    df['status'] = df['status'].replace({'Left': 1, 'Employed': 0})
+
+    # Split into covariates, labels
+    labels = df['status'].values.astype(int)
+    covariates = df.drop('status', axis=1)
+
+
+    # Create cost matrix
+    n_samples = labels.shape[0]
+    cost_matrix = np.zeros((n_samples, 2, 2))     # cost_matrix [[TN, FN], [FP, TP]]
+    cost_matrix[:, 0, 0] = 0
+    cost_matrix[:, 0, 1] = 1
+    cost_matrix[:, 1, 0] = 1
+    cost_matrix[:, 1, 1] = 0
+
+    # List categorical variables
+    categorical_variables = ['department','salary',]
+
+    amounts = np.ones((n_samples))
+
+    return covariates, labels, amounts, cost_matrix, categorical_variables
 
 def preprocess_ibm():
 
